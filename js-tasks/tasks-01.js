4b19379202a9c57e2f87696f0d6da784bc4b0f5b
yuliaivashko <yuliaivashko3@gmail.com>
2024-09-20 19:34:18 +0300

    Create tasks-01.js

---
+++ js-tasks/tasks-01.js
@@ -0,0 +1,4 @@
+// https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
