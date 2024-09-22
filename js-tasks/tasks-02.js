527b425f340c795bdd440a7927d495217ca804d0
ocheresh <51712586+ocheresh@users.noreply.github.com>
2024-09-22 10:38:55 +0000

    use prettier in file tasks2

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,34 +1,30 @@
 //https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
 
-function stringToArray(string){
-  return arr = string.split(" ");
+function stringToArray(string) {
+  return (arr = string.split(" "));
 }
-
-
 
 //https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
 
 function DNAtoRNA(dna) {
-  return dna.replace(/T/g, "U");;
+  return dna.replace(/T/g, "U");
 }
-
 
 //https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 
-var min = function(list){
-    list.sort((a, b) => a - b);
-    return list[0];
-}
+var min = function (list) {
+  list.sort((a, b) => a - b);
+  return list[0];
+};
 
-var max = function(list){
-    list.sort((a, b) => b - a);
-    return list[0];
-}
-
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list[0];
+};
 
 //https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 
 function min(arr, toReturn) {
   const minValue = Math.min(...arr);
-  return toReturn === 'value' ? minValue : arr.indexOf(minValue);
+  return toReturn === "value" ? minValue : arr.indexOf(minValue);
 }
