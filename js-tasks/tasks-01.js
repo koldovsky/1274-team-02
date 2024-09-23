6f71657b34ce9cb3d22062e6e4627626d2023729
iragni <111970829+iragni@users.noreply.github.com>
2024-09-23 12:04:35 +0300

    Update tasks-01.js

--- js-tasks/tasks-01.js
+++ js-tasks/tasks-01.js
@@ -30,3 +30,8 @@ function lovefunc(flower1, flower2) {
     ? false
     : true;
 }
+
+//https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/javascript
+String.prototype.toJadenCase = function () {
+    return this.split(' ').map(word =>word.charAt(0).toUpperCase() + word.slice(1)).join(' ');                 
+};
