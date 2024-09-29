e689c536b7a5e6c30cb3e09777f3af2565e84132
Markian-Grybok <grybokmarkian@gmail.com>
2024-09-29 23:15:53 +0000

    add small animation in js to my partial

---
+++ js/index.section-join-the-game.js
@@ -0,0 +1,36 @@
+const title = document.querySelector(".join-the-game__title");
+const points = document.querySelector(".join-the-game__point");
+const subtitle = document.querySelector(".join-the-game__subtitle");
+
+const titleColor = ["#c1305c", "white", "#fcee79", "#a94b9c"];
+
+const pointsCount = ["●", "● ●", "● ● ●"];
+
+const subtitleText =
+  "Want to be the first to know about our releases and discounts? Subscribeto our newsletter! ";
+
+function changeColor(arr, celtor) {
+  celtor.style.color = arr[Math.floor(Math.random() * arr.length)];
+}
+
+let currentIndexPoints = 0;
+
+function changePoints(arr) {
+  points.innerText = arr[currentIndexPoints++];
+  if (currentIndexPoints >= arr.length) currentIndexPoints = 0;
+}
+
+let currentIndexWords = 999;
+const wordsHolder = 50;
+
+function writeSubtitleWords(arr) {
+  if (currentIndexWords >= arr.length + wordsHolder) {
+    currentIndexWords = 0;
+    subtitle.innerText = "";
+  } else if (currentIndexWords >= arr.length) currentIndexWords++;
+  else subtitle.innerHTML += arr[currentIndexWords++];
+}
+
+setInterval(() => changeColor(titleColor, title), 800);
+setInterval(() => changePoints(pointsCount), 600);
+setInterval(() => writeSubtitleWords(subtitleText), 20);
